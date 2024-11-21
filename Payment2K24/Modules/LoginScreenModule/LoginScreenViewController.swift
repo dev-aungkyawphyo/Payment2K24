4fd51ef10b42c7db416041db739e882168deac03
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-11-21 23:13:31 +0630

    initial commit

---
+++ Payment2K24/Modules/LoginScreenModule/LoginScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginScreenViewController.swift
+//  Payment2K24
+//
+//  Created by Aung Kyaw Phyo on 21/11/2024.
+//
+
+import UIKit
+
+class LoginScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
